fda7520cf9daefb6019907439068a87badb6efa0
chema <jomoro01@floridauniversitaria.es>
2020-10-30 19:38:54 +0100

    cambios

--- WepApi1/WepApi1/Models/ApuestasRepository.cs
+++ WepApi1/WepApi1/Models/ApuestasRepository.cs
@@ -1,34 +1,39 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿//using MySql.Data.MySqlClient;
+//using System;
 using System.Collections.Generic;
-using System.Diagnostics;
+//using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
-using System.Web;
+//using System.Web;
 
 namespace WepApi1.Models
 {
     public class ApuestasRepository
     {
-        private MySqlConnection Connect()
+      /*  private MySqlConnection Connect()
         {
             string connString = "Server=127.0.0.1;Port=3306;Database=placemybet;Uid=root;SslMode=none";
             MySqlConnection con = new MySqlConnection(connString);
             return con;
 
-        }
+        }*/
         internal List<Apuesta> Retrieve()
         {
-            MySqlConnection con = Connect();
-            MySqlCommand command = con.CreateCommand();
-            command.CommandText = "select * from Apuestas";
-            try
-            {
-                con.Open();
-                MySqlDataReader res = command.ExecuteReader();
-                Apuesta a = null;
+           // MySqlConnection con = Connect();
+           // MySqlCommand command = con.CreateCommand();
+           // command.CommandText = "select * from Apuestas";
+            
+            
+              //  con.Open();
+              //  MySqlDataReader res = command.ExecuteReader();
+               // Apuesta a = null;
                 List<Apuesta> apuestas = new List<Apuesta>();
-                while (res.Read())
+                using (ApuestasContext context = new ApuestasContext())
+                {
+                    apuestas = context.Apuestas.ToList();
+
+                }
+               /* while (res.Read())
                 {
                     Debug.WriteLine("recuperamosapuestas: " + res.GetInt32(0) + " " + res.GetString(1)
                                     + " " + res.GetInt32(2) + " " + res.GetString(3) + " " +
@@ -36,16 +41,16 @@ namespace WepApi1.Models
                     a = new Apuesta(res.GetInt32(0), res.GetString(1), res.GetInt32(2), res.GetString(3), res.GetFloat(4), res.GetInt32(5), res.GetDateTime(6));
                     apuestas.Add(a);
                 }
-                con.Close();
+                con.Close();*/
                 return apuestas;
-            }
-            catch (MySqlException e)
+            
+            /*catch (MySqlException e)
             {
                 Debug.WriteLine("se ha producido un error de conexión");
                 return null;
 
 
-            }
+            }*/
 
         }
         internal List<Apuesta> RetrieveByUsuarioandId_mercado(string usuario,int id)
